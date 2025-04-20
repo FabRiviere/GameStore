098b62b98fbca92e7e7ee2fc78bf7382ac754a60
Fab Riviere <fabien@activateurweb.com>
2025-04-20 16:29:58 +0200

    plugin init

---
+++ plugins/core-gamestore/core-gamestore.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name: Core GameStore
+ * Description: Core Code for GameStore.
+ * Version: 1.0.0
+ * Author: Activateur.Web
+ * Author URI: https://activateurweb.com
+ * License: GPL2
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: core-gamestore
+ * Domain Path: /languages
+ */
+
+ define('GAMESTORE_PLUGIN', plugin_dir_url(__FILE__));
+ define('GAMESTORE_PLUGIN_PATH', plugin_dir_path(__FILE__));
